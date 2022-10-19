2ca9f789d5c3c08846ccd63f9bc094bc5c57142d
ALEKSEY KIM <alex.romanovich.ak@gmail.com>
2022-10-20 02:22:17 +0900

    add bg music and other audio files

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
-
+class AudioController {
+    constructor() {
+        this.bgMusic = new Audio('assets/Audio/creepy.mp3');
+        this.flipSound = new Audio('assets/Audio/flip.wav');
+        this.matchSound = new Audio('assets/Audio/match.wav');
+        this.victorySound  = new Audio('assets/Audio/victory.wav');
+        this.gameOverSound  = new Audio('assets/Audio/gameOver.wav');
+        this.bgMusic.volume = 0.5;
+        this.bgMusic.loop = true;
+    }
+    startMusic() {
+       this.bgMusic.play(); 
+    }
+    stopMusic() {
+        this.bgMusic.pause();
+        this.bgMusic.currentTime = 0;
+    }
+    flip() {
+        this.flipSound.play();
+    }
+    match() {
+        this.matchSound.play();
+    }
+    victory() {
+        this.stopMusic();
+        this.victory.play();
+    }
+    gameOver() {
+        this.stopMusic();
+        this.gameOverSound.play();
+    }
+}
 
 function ready() {
     let overlays = Array.from(document.getElementsByClassName('overlay-text'));
@@ -8,6 +39,9 @@ function ready() {
         overlay.addEventListener('click', () => {
             overlay.classList.remove('visible');
             //game.startGame();
+            // let audioController = new AudioController();
+            // audioController.startMusic();
+
         });
     });
 
@@ -23,3 +57,4 @@ if(document.readyState === 'loading') {
 } else {
     ready()
 }
+
